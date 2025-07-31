e15f3bc1e796fe05056cb5249793aa9605f6a1d3
SurajP5697 <surajspadase12@gmail.com>
2025-07-31 14:44:33 +0530

    Updated App file

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Hi")
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello from Node.js App\n');
+});
+
+server.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
